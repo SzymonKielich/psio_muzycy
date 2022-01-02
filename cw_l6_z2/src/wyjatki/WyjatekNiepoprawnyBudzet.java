8f5c46082f6c9aed1072834c99c48c108bc8b331
datweirdo25 <95685479+datweirdo25@users.noreply.github.com>
2022-01-02 18:59:52 +0100

    wyjatki

--- cw_l6_z2/src/wyjatki/WyjatekNiepoprawnyBudzet.java
+++ cw_l6_z2/src/wyjatki/WyjatekNiepoprawnyBudzet.java
@@ -1,5 +1,10 @@
 package wyjatki;
 
-public class WyjatekNiepoprawnyBudzet {
+public class WyjatekNiepoprawnyBudzet extends Exception{
+	
+	public String getMessage()
+	{
+		return super.getMessage() + " Kwota wprowadzonego budzetu powinna byc wielokrotnoscia 100";
+	}
 
 }
