14d1fc8c9b481c5854d0426b9e1fc9b2447f8768
komp15 <michal.komp@interia.eu>
2022-01-16 12:16:35 +0100

    maven, junit, and mockito implementation

---
+++ cw_l6_z2/test/cw_l6_z2/CzyKazdyMuzykMaWszystkieKryteria.java
@@ -0,0 +1,19 @@
+package cw_l6_z2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CzyKazdyMuzykMaWszystkieKryteria {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testDane_initializeDatabase() {
+		fail("Not yet implemented");
+	}
+
+}
