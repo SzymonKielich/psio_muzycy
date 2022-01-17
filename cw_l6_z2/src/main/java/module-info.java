f9577b6df29f9754b15294b115abd1647fc50e8b
komp15 <77535280+komp15@users.noreply.github.com>
2022-01-17 23:59:09 +0100

    added module-info.java files

---
+++ cw_l6_z2/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module cw_l6_z2 {
+    requires java.desktop;
+	requires junit;
+}
--- cw_l6_z2/src/module-info.java
+++ cw_l6_z2/src/module-info.java
@@ -1,4 +1,4 @@
-module cw_l6_z2/src/main {
+module cw_l6_z2/src {
     requires java.desktop;
 	requires junit;
 }
